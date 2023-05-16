eadbbea164633bca928d79c837fb679ebb70bc00
shljessie <sl994@cornell.edu>
2023-05-16 10:14:48 +0300

    udpate remove_dup

---
+++ practice/leetcode_easy/remove_dup.py
@@ -0,0 +1,39 @@
+"""
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+"""
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]: 
+                nums[ans] = nums[i] 
+                ans += 1
+
+        print(nums)
+        return ans
+
+"""
+Sample INput output 
+
+nums =
+[1,1,2]
+
+final = 
+[1, 2, 2]
+
+since ans =2 
+we parse the first 2 elements of the nums array
+and get [1,2]
+
+interesting...
+"""
