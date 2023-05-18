3a2af06ac0b539dd92865da81e25cb0166b5977e
shljessie <sl994@cornell.edu>
2023-05-18 09:48:36 +0300

    updatelinkedlist

---
+++ practice/leetcode_easy/linked_list/reverse_list.py
@@ -0,0 +1,13 @@
+"""
+Given the head of a singly linked list, 
+reverse the list, and return the reversed list.
+
+
+LinkedList 관해서 정리하기
+"""
+
+def reverseList(self, head):
+    """
+    :type head: ListNode
+    :rtype: ListNode
+    """
---
+++ practice/leetcode_easy/string/reverseString.py
@@ -0,0 +1,23 @@
+"""
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+
+reverse() actually reverses the elements in the container. reversed() 
+doesn't actually reverse anything, it merely returns an object that can 
+be used to iterate over the container's elements in reverse order.
+
+"""
+
+# reversed(list) => returns an object but doesn't really reverse the original string
+# list.reverse() reverses the list 
+
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        return s.reverse()
